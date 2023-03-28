08fbd3465552a6c8f2980a5354a282b86ebeed80
Rodevp <rodriguezrodrigor12@gmail.com>
2023-03-28 16:09:46 -0500

    add colors

---
+++ theme.js
@@ -0,0 +1,10 @@
+export default {
+    colors: {
+        background: '#FFFFFC',
+        green: '#84DCC6',
+        greenLigth: '#A5FFD6',
+        red: '#FF686B',
+        textColor: '#212529',
+        orange: '#FFD670'
+    }
+}
