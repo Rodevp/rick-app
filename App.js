9862f12f4b1ee528c59ab40b37c3dc225017613c
Rodevp <rodriguezrodrigor12@gmail.com>
2023-03-28 17:18:24 -0500

    add navigation dependecies

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
 import Home from './screens/Home';
+
+//<StatusBar style="auto" />
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
+    <>
       <Home />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,16 +1,19 @@
 import { View, StyleSheet } from 'react-native'
+import Header from '../widgets/Header'
 
 export default function Home() {
   return (
     <View
         style={styles.home}
     >
+        <Header />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
     home: {
-        
+        flex: 1,
+        backgroundColor: "red"
     }
 })
