4fdc8ebbac73adedeec0ee8230cf25cecaedd3c3
Rodevp <rodriguezrodrigor12@gmail.com>
2023-03-28 20:38:12 -0500

    navigation flow complete

--- screens/Episodes.jsx
+++ screens/Episodes.jsx
@@ -1,10 +1,29 @@
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { View, Text } from 'react-native'
+import { Text, StyleSheet } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import BackHeader from '../widgets/BackHeader'
+import theme from '../theme'
 
 export default function Episodes() {
+
+  const navigation = useNavigation()
+
   return (
-    <SafeAreaView>
+    <SafeAreaView
+      style={styles.container}
+    >
+      <BackHeader
+        navigation={navigation}
+      />
       <Text>Episodes</Text>
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+    backgroundColor: theme.colors.background
+  }
+})
--- screens/Favs.jsx
+++ screens/Favs.jsx
@@ -1,11 +1,30 @@
-import { Text } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { Text, StyleSheet } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import theme from '../theme'
+import BackHeader from '../widgets/BackHeader'
 
 
 export default function Favs() {
+
+  const navigation = useNavigation()
+
   return (
-    <SafeAreaView>
+    <SafeAreaView
+      style={styles.container}
+    >
+      <BackHeader
+        navigation={navigation}
+      />
       <Text>Favs</Text>
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+    backgroundColor: theme.colors.background
+  }
+})
